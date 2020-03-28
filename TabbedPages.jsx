13c7ed37664508c6532b7c7401318651d8b21219
Albert Santos <leeboardtools@gmail.com>
2020-03-27 22:38:00 -0400

    Started menu restructuring to use all HTML for menus

--- TabbedPages.jsx
+++ TabbedPages.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { DropDown } from './DropDown';
 
 
 
@@ -33,9 +34,20 @@ export class TabbedPages extends React.Component {
 
     renderTabs() {
         const items = this.props.tabEntries.map((tabEntry) => {
+            const ariaLabel = tabEntry.title + ' Tab';
+
             let className = 'nav-item nav-link tabText';
+            let tabComponent;
             if (tabEntry.tabId === this.state.activeTabId) {
                 className += ' active';
+            }
+
+            if (!tabComponent) {
+                tabComponent = <a href="#" onClick={() => this.handleTabClick(tabEntry)}
+                    aria-label={ariaLabel}
+                >
+                    {tabEntry.title}
+                </a>;
             }
 
             let closeButton;
@@ -49,20 +61,73 @@ export class TabbedPages extends React.Component {
                 </button>;
             }
 
-            const ariaLabel = tabEntry.title + ' Tab';
             return (
                 <div key={tabEntry.tabId} className={className}>
-                    <a href="#" onClick={() => this.handleTabClick(tabEntry)}
-                        aria-label={ariaLabel}
-                    >
-                        {tabEntry.title}
-                    </a>
+                    {tabComponent}
                     {closeButton}
                 </div>
             );
         });
 
-        let className = this.props.tabClassName || 'nav nav-tabs fixed-top bg-light';
+        if (!items) {
+            // The format is:
+            //  <nav className="nav nav-tabs bg-light">
+            //      <div className="nav-item nav-link tabText">
+            //          <a href="#"...>{tabEntry.title}
+            //          <button type="button" className="close tabCloseButton">
+            //              <span aria-hidden="true">&times;</span>
+            //          </button>
+            //      </div>
+            //      ...
+            //      ...
+            //  </nav>
+            const menuItems = [
+                { id: 'a', label: 'A', },
+                { id: 'b', label: 'B', disabled: true, },
+                { },
+                { id: 'c', label: 'C', },
+            ];
+            const menu = <DropDown title="Menu"
+                id = "Abc"
+                buttonClassExtras="btn btn-primary"
+                items = { menuItems}
+                onChooseItem={(id) => { console.log('choose: ' + id)}}
+            />;
+/*
+            const menu = <div className="">
+                <div className="dropdown">
+                    <button className="btn btn-secondary dropdown-toggle"
+                        type="button"
+                        id="dropdownMenuButton"
+                        data-toggle="dropdown"
+                        aria-haspopup="true"
+                        aria-expanded="false"
+                    >
+                        My Menu
+                    </button>
+                    <div className="dropdown-menu"
+                        aria-labelledby="dropdownMenuButton"
+                    >
+                        <a className="dropdown-item" href="#">Item A</a>
+                        <a className="dropdown-item" href="#">Item B</a>
+                        <a className="dropdown-item" href="#">Item C</a>
+                    </div>
+                </div>
+            </div>;
+*/
+            const navBar = <nav className="nav nav-tabs bg-light">
+                {items}
+            </nav>;
+
+            return <div className="d-flex">
+                <div className="flex-grow-1">
+                    {navBar}
+                </div>
+                {menu}
+            </div>;
+        }
+
+        let className = this.props.tabClassName || 'nav nav-tabs bg-light';
         if (this.props.tabExtras) {
             className += ' ' + this.props.tabExtras;
         }
@@ -90,10 +155,16 @@ export class TabbedPages extends React.Component {
 
 
     render() {
-        const tabs = this.renderTabs();
+        let tabs = this.renderTabs();
+
+        const { onPostRenderTabs } = this.props;
+        if (onPostRenderTabs) {
+            tabs = onPostRenderTabs(tabs);
+        }
+
         const body = this.renderBody();
 
-        return <div className="container-fluid tabPage pl-1 pr-1">
+        return <div className="container-fluid pl-1 pr-1">
             {tabs}
             {body}
         </div>;
@@ -126,6 +197,13 @@ export class TabbedPages extends React.Component {
  */
 
 /**
+ * @callback {TabbedPages~onPostRenderTabs}
+ * @param {object}    tabs  The component representing the tabs.
+ * @return {object} The component to render, this would normally be a parent component
+ * around tabs.
+ */
+
+/**
  * @typedef {object}    TabbedPages~PropTypes
  * @property {TabbedPages~TabEntry[]}   tabEntries  The array of the tab entries.
  * @property {string}   [activeTabId]   The tabId of the active tab entry.
@@ -136,6 +214,12 @@ export class TabbedPages extends React.Component {
  * @property {TabbedPages~onActivateTab}    [onActivateTab] Called when a tab's
  * page becomes active. This is only called when the tab is chosen, not for
  * the initially active page.
+ * @property {string}   [tabClassName]  Initial class for individual tabs, Bootstrap
+ * classes added to this.
+ * @property {string}   [tabExtras] Extra classes to add for individual tabs after
+ * the Bootstrap classes.
+ * @property {TabbedPages~onPostRenderTabs} [onPostRenderTabs] Callback that allows
+ * processing of the tabs component to render.
  */
 TabbedPages.propTypes = {
     tabEntries: PropTypes.array.isRequired,
@@ -145,4 +229,5 @@ TabbedPages.propTypes = {
     onActivateTab: PropTypes.func,
     tabClassName: PropTypes.string,
     tabExtras: PropTypes.string,
+    onPostRenderTabs: PropTypes.func,
 };
