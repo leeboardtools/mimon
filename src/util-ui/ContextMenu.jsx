a536db2ffec1aa32b3f53be64c9fbc27eff7f1cc
Albert Santos <leeboardtools@gmail.com>
2020-12-27 10:27:59 -0500

    Added scrollable-menu

--- src/util-ui/ContextMenu.jsx
+++ src/util-ui/ContextMenu.jsx
@@ -128,7 +128,7 @@ export class ContextMenu extends React.Component {
             menuClassExtras += ' show';
             menuComponent = <MenuList
                 items={items}
-                menuClassExtras={menuClassExtras}
+                menuClassExtras={'scrollable-menu ' + menuClassExtras}
                 itemClassExtras={itemClassExtras}
                 onChooseItem={onChooseItem}
                 ref={this._menuRef}
