0d9a2bcc219e59a3b7b05bac5b1278d34daba28a
Albert Santos <leeboardtools@gmail.com>
2020-12-29 14:35:26 -0500

    Added accountEntriesToItems(), addAccountIdsToAccountEntries(), support for direct items from accountEntriesToItems(), prepend/append component props

--- src/ui/AccountSelector.jsx
+++ src/ui/AccountSelector.jsx
@@ -5,44 +5,119 @@ import { userMsg } from '../util/UserMessages';
 
 
 /**
+ * @typedef {object} addAccountIdsToAccountEntriesFilterCallback~Result
+ * @property {boolean} [skipAccountId]
+ * @property {boolean} [skipChildren]
+ * @property {boolean} [disabled]
+ */
+
+
+/**
+ * Filter callback for {@link addAccountIdsToAccountEntries}
+ * @callback addAccountIdsToAccountEntriesFilterCallback
+ * @param {number} accountId
+ * @returns {addAccountIdsToAccountEntriesFilterCallback~Result|undefined}
+ */
+
+/**
+ * Helper for building the accountEntries property for {@link AccountSelector}.
+ * @param {EngineAccessor} accessor 
+ * @property {AccountSelector~AccountEntry} accountEntries
+ * @param {number} accountId 
+ * @param {addAccountIdsToAccountEntriesFilterCallback} filter 
+ */
+export function addAccountIdsToAccountEntries(accessor, accountEntries,
+    accountIds, filter) {
+
+    filter = filter || (() => undefined);
+
+    accountIds.forEach((accountId) => {
+        const filterResult = filter(accountId) || {};
+        if (!filterResult.skipAccountId) {
+            const accountEntry = {
+                accountId: accountId,
+            };
+            if (filterResult.disabled) {
+                accountEntry.disabled = true;
+            }
+            accountEntries.push(accountEntry);
+        }
+        if (!filterResult.skipChildren) {
+            const accountDataItem = accessor.getAccountDataItemWithId(accountId);
+            if (accountDataItem && accountDataItem.childAccountIds) {                    
+                addAccountIdsToAccountEntries(accessor, accountEntries,
+                    accountDataItem.childAccountIds, filter);
+            }
+        }
+    });
+}
+
+
+/**
+ * Converts an array of {@link AccountSelector~AccountEntry}s into items that
+ * can be used directly by {@link AccountSelector} with the accountEntriesAreItems
+ * property set to <code>true</code>.
+ * <p>
+ * This is to avoid rebuilding the item list each time the selector is rendered.
+ * @param {EngineAccessor} accessor 
+ * @param {AccountSelector~AccountEntry} accountEntries
+ * @returns {Array}
+ */
+export function accountEntriesToItems(accessor, accountEntries) {
+    const items = [];
+    const parentStack = [];
+    accountEntries.forEach((entry) => {
+        const { accountId } = entry;
+        while (parentStack.length) {
+            const parent = parentStack[parentStack.length - 1];
+            if (parent.childAccountIds 
+            && (parent.childAccountIds.indexOf(accountId) >= 0)) {
+                break;
+            }
+            else {
+                --parentStack.length;
+            }
+        }
+
+        const accountDataItem = accessor.getAccountDataItemWithId(accountId);
+        const item = {
+            value: accountId,
+            text: entry.text || accountDataItem.name,
+            indent: parentStack.length,
+        };
+        if (entry.disabled) {
+            item.disabled = true;
+        }
+        
+        items.push(item);
+
+        parentStack.push(accountDataItem);
+    });
+
+    return items;
+}
+
+
+/**
  * Dropdown field component for selecting an account.
  * @class
  */
 export function AccountSelector(props) {
-    const { accessor, accountEntries, selectedAccountId, 
-        disabledRoot,
+    const { accessor, accountEntries, accountEntriesAreItems,
+        selectedAccountId, disabledRoot,
         ...passThroughProps } = props;
-    const items = [];
+    let items = [];
     if (disabledRoot) {
         items.push({
             value: selectedAccountId,
             text: userMsg('AccountSelector-disabled_root'),
         });
     }
+    else if (accountEntriesAreItems) {
+        items = accountEntries;
+    }
     else {
-        const parentStack = [];
-        accountEntries.forEach((entry) => {
-            const { accountId } = entry;
-            while (parentStack.length) {
-                const parent = parentStack[parentStack.length - 1];
-                if (parent.childAccountIds 
-                && (parent.childAccountIds.indexOf(accountId) >= 0)) {
-                    break;
-                }
-                else {
-                    --parentStack.length;
-                }
-            }
-
-            const accountDataItem = accessor.getAccountDataItemWithId(accountId);
-            items.push({
-                value: accountId,
-                text: accountDataItem.name,
-                indent: parentStack.length,
-            });
-
-            parentStack.push(accountDataItem);
-        });
+        items = accountEntriesToItems(accessor, accountEntries);
     }
 
     return <DropdownField
@@ -59,11 +134,15 @@ export function AccountSelector(props) {
 
 /**
  * @typedef {object}    AccountSelector~propTypes
+ * @property {AccountSelector~AccountEntry} accountEntries
+ * @property {boolean} [accountEntriesAreItems=false] Set to true if
+ * accountEntries has been converted to items via {@link accountEntriesToItems}.
  */
 AccountSelector.propTypes = {
     accessor: PropTypes.object.isRequired,
     id: PropTypes.string,
     accountEntries: PropTypes.array.isRequired,
+    accountEntriesAreItems: PropTypes.bool,
     ariaLabel: PropTypes.string,
     label: PropTypes.string,
     selectedAccountId: PropTypes.number,
@@ -74,4 +153,12 @@ AccountSelector.propTypes = {
     onBlur: PropTypes.func,
     disabled: PropTypes.oneOfType([PropTypes.bool, PropTypes.number]),
     disabledRoot: PropTypes.oneOfType([PropTypes.bool, PropTypes.number]),
+    prependComponent: PropTypes.oneOfType([
+        PropTypes.object,
+        PropTypes.string,
+    ]),
+    appendComponent: PropTypes.oneOfType([
+        PropTypes.object,
+        PropTypes.string,
+    ]),
 };
