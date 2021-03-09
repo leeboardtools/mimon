c3ec6a89c87861733381646d46baf59efb87fb25
Albert Santos <leeboardtools@gmail.com>
2021-03-09 17:19:22 -0500

    Replaced BS d-flex with FlexC, flex-grow- with FlexI-grow-, flex-column with FlexC-column

--- ContentFramer.jsx
+++ ContentFramer.jsx
@@ -136,7 +136,7 @@ export class ContentFramer extends React.Component {
         const { classExtras } = this.props;
 
         let className = 'container-fluid p-0'
-            + ' d-flex flex-column h-100 overflow-hidden ContentFramer-container';
+            + ' FlexC FlexC-column h-100 overflow-hidden ContentFramer-container';
         if (classExtras) {
             className += ' ' + classExtras;
         }
